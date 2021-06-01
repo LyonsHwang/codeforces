daae569d4f45a045d5d24111999bfc9ae294364d
Promising <61009012+LyonsHwang@users.noreply.github.com>
2021-06-01 20:33:58 +0800

    Create TEMPLATE.cpp
    基础模板, 可以写出像诗歌一样的代码.

---
+++ TEMPLATE.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define lyons ;
+#define fulcrum(...) ;
+#ifndef promising
+#define dump(...)
+#endif
+
+#define E 2.718281828459045
+#define PI 3.141592653589793
+#define M1 167772161
+#define M3 998244353
+#define M7 1000000007
+#define INF 0x3fffffff
+#define INF64 0x3fffffff3fffffff
+
+const int P=M3;
+
+#define loop(i,n) for(int i=0;i<(n);i++)
+#define lback(i,n) for(int i=(n)-1;i>=0;i--)
+#define rep(i,l,r) for(int i=(l);i<(r);i++)
+#define rback(i,l,r) for(int i=(r)-1;i>=(l);i--)
+#define all(x) (x).begin(),(x).end()
+#define tri(op,a,b,c) op(op(a,b),c)
+#define isz(x) ((int)(x).size())
+#define iln(x) ((int)(x).length())
+
+#define mid (l+(r-l)/2)
+#define lson (x<<1)
+#define rson (x<<1|1)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,int> pli;
+typedef pair<long long,long long> pll;
+typedef complex<double> cp;
+template<class T> using vector2D = vector<vector<T>>;
+template<class T> using vector3D = vector<vector2D<T>>;
+
+template<class T> bool umax(T &x, T y) { return (x<y)? x=y,true: false; }
+template<class T> bool umin(T &x, T y) { return (x>y)? x=y,true: false; }
+
+void prepare() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+int main() {
+    prepare();
+
+    return 0;
+}
