f4069197ac078e68645351a6cb5be2d85cacbdc1
Promising <61009012+LyonsHwang@users.noreply.github.com>
2021-09-09 16:16:59 +0800

    Update 数据结构 线段树·可并.cpp

--- code/数据结构 线段树·可并.cpp
+++ code/数据结构 线段树·可并.cpp
@@ -8,42 +8,65 @@ struct MSeg {
         Ln=l; Rn=r;
         t.assign(1,{0,0,0});
     }
+    int bld(int l,int r){ 
+        int x=add();
+        if(stop) t[x].sum=1;
+        else {
+            t[x].l=bld(l,mid);
+            t[x].r=bld(mid,r);
+            push(x);
+        }
+        return x;
+    }
+    int build(int l,int r) {
+        return init(l,r),bld(l,r);
+    }
     int add() {
-        t.resize(t.size()+1);
+        t.push_back(t[0]);
         return t.size()-1;
     }
-    void pushup(int x) {
+    void push(int x) {
         t[x].sum=t[t[x].l].sum+t[t[x].r].sum;
     }
     int upd(int o,int l,int r) {
+        if(G<l||G>=r) return o;
         int x=add(); t[x]=t[o];
-        if(l==r) t[x].sum=V;
+        if(l+1==r) t[x].sum=V;
         else {
-            int mid=(l+r)>>1;
-            if(G<=mid) t[x].l=upd(t[x].l,l,mid);
-            else t[x].r=upd(t[x].r,mid+1,r);
-            pushup(x);
+            t[x].l=upd(t[x].l,l,mid);
+            t[x].r=upd(t[x].r,mid,r);
+            push(x);
         }
         return x;
     }
     int update(int o,int g,int v) {
         return G=g, V=v, upd(o,Ln,Rn);
     }
+    int cov(int o1,int o2,int l,int r) {
+        if(!o1||r<=L||l>=R) return o2;
+        if(L<=l&&r<=R) return o1;
+        int x=add();
+        t[x].l=cov(t[o1].l,t[o2].l,l,mid);
+        t[x].r=cov(t[o1].r,t[o2].r,mid,r);
+        return push(x), x;
+    }
+    int cover(int o1,int o2,int l,int r) {
+        return L=l,R=r,cov(o1,o2,Ln,Rn);
+    }
     int merge(int o1,int o2) {
         if(!o1||!o2) return o1+o2;
-        int x=add(); {
-            int l=merge(t[o1].l,t[o2].l);
-            int r=merge(t[o1].r,t[o2].r);
-            int sum=t[o1].sum+t[o2].sum;
-            t[x]={l,r,sum};
-        }
+        int x=add();
+        t[x].l=merge(t[o1].l,t[o2].l);
+        t[x].r=merge(t[o1].r,t[o2].r);
+        t[x].sum=t[o1].sum+t[o2].sum;
         return x;
     }
     int qry(int x,int l,int r) {
+        if(!x) return 0;
         if(L<=l&&r<=R) return t[x].sum;
-        int mid=(l+r)>>1,res=0;
-        if(L<=mid) res+=qry(t[x].l,l,mid);
-        if(R>mid) res+=qry(t[x].r,mid+1,r);
+        int res=0;
+        if(L<mid) res+=qry(t[x].l,l,mid);
+        if(R>mid) res+=qry(t[x].r,mid,r);
         return res;
     }
     int query(int x,int l,int r) {
