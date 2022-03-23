0c417d615dbc79abadd83540751dc12a7caacdab
Promising <61009012+LyonsHwang@users.noreply.github.com>
2022-03-24 02:10:31 +0800

    Create KmpAlgorithm.cpp

---
+++ code_v2/string/KmpAlgorithm.cpp
@@ -0,0 +1,23 @@
+struct KMP {
+    int n;
+    vector<int> s;  // string
+    vector<int> b;  // border
+    void init() {
+        n = ::n;
+        s.assign(::s, ::s + n);
+        b.resize(n);
+    }
+    void build() {
+        for (int j = 0, i = 1; i < n; i++) {
+            while (j > 0 && s[j] != s[i]) j = b[j - 1];
+            if (s[j] == s[i]) ++j;
+            b[i] = j;
+        }
+    }
+    int trans(int k, int c) {
+        if (k == n) k = b[k - 1];
+        while (k > 0 && s[k] != c) k = b[k - 1];
+        if (s[k] == c) ++k;
+        return k;
+    }
+} kmp;
