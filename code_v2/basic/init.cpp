098f0fbc3f76c53570c158adbc1ebd821a3b7069
Promising <61009012+LyonsHwang@users.noreply.github.com>
2022-03-27 19:46:25 +0800

    Update init.cpp

--- code_v2/basic/init.cpp
+++ code_v2/basic/init.cpp
@@ -1,10 +1,8 @@
-#include <bits/stdc++.h>
 #ifndef promising
 #define dump(...)
 #endif
-#define M1 167772161
-#define M3 998244353
-#define M7 1000000007
+
+#include <bits/stdc++.h>
 #define isz(x) ((int)(x).size())
 #define iln(x) ((int)(x).length())
 #define all(x) (x).begin(), (x).end()
@@ -12,8 +10,8 @@
 #define lback(i, n) for (int i = (n)-1; i >= 0; i--)
 #define rep(i, l, r) for (int i = (l); i < (r); i++)
 #define rback(i, l, r) for (int i = (r)-1; i >= (l); i--)
+
 using namespace std;
-using ll = long long;
 template <class T> using alpha = array<T, 26>;
 template <class T> using vector2D = vector<vector<T>>;
 template <class T> using vector3D = vector<vector2D<T>>;
@@ -22,7 +20,15 @@ template <class T> using heap = priority_queue<T, vector<T>, greater<T>>;
 template <class T> bool umax(T &x, T y) { return x < y ? x = y, true : false; }
 template <class T> bool umin(T &x, T y) { return x > y ? x = y, true : false; }
 void prepare() { ios::sync_with_stdio(0), cin.tie(0), cout.tie(0); }
+using ll = long long;
+
+#define M1 167772161
+#define M3 998244353
+#define M7 1000000007
+const int P = M3;
+
 int main() {
     prepare();
+
     return 0;
 }
