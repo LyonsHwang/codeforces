6e2977510e85c2539727c72d26dd07a9f288f1ff
Promising <61009012+LyonsHwang@users.noreply.github.com>
2022-04-07 14:59:06 +0800

    Create EratosthenesSieve.cpp

---
+++ code_v2/math/EratosthenesSieve.cpp
@@ -0,0 +1,16 @@
+struct EratosthenesSieve {
+    vector<int> ps;
+    vector<int> mn;
+    void init(int n) {
+        ps.clear();
+        mn.assign(n, 0);
+        rep(i, 2, n) {
+            if (mn[i] == 0) ps.push_back(i);
+            for (auto p : ps) {
+                if (i * p >= n) break;
+                mn[i * p] = p;
+                if (i % p == 0) break;
+            }
+        }
+    }
+};
