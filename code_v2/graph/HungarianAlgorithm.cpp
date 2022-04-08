103b9a99a878deb9f39b12c1a5bc5340c27d6123
Promising <61009012+LyonsHwang@users.noreply.github.com>
2022-04-08 14:21:17 +0800

    Update HungarianAlgorithm.cpp

--- code_v2/graph/HungarianAlgorithm.cpp
+++ code_v2/graph/HungarianAlgorithm.cpp
@@ -1,29 +1,27 @@
-// int n1, n2;
+// int n;
 // vector<int> g[maxn];
 namespace Hungarian {
 int m;        // match count
-int a[maxn];  // vertex match
-int b[maxn];  // vertex match
-int c[maxn];  // visited
+int a[maxn];  // match
+int c[maxn];  // visit
 bool find(int x) {
     if (c[x]) return 0;
     c[x] = 1;
     for (auto y : g[x])
-        if (b[y] == -1 || find(b[y])) {
-            b[y] = x;
-            a[x] = y;
+        if (a[y] == -1 || find(a[y])) {
+            a[y] = x, a[x] = y;
             return true;
         }
     return false;
 }
+bool augment(int x) {
+    loop(i, n) c[i] = 0;
+    return find(x);
+}
 int match() {
     m = 0;
-    loop(i, n1) a[i] = -1;
-    loop(i, n2) b[i] = -1;
-    loop(i, n1) {
-        loop(j, n1) c[j] = 0;
-        m += find(i);
-    }
+    loop(i, n) a[i] = -1;
+    loop(i, n) if (a[i] == -1) m += augment(i);
     return m;
 }
 }  // namespace Hungarian
